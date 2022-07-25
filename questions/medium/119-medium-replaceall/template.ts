b68539fe595c6b5996c1649f8647191a393ddbb5
Kevin Ingles <kfabian@applaudostudios.dev>
2022-07-24 21:05:06 -0600

    ✨ Add replace all challenge

--- questions/medium/119-medium-replaceall/template.ts
+++ questions/medium/119-medium-replaceall/template.ts
@@ -1,5 +1,10 @@
 export type ReplaceAll<
   S extends string,
   From extends string,
-  To extends string
-> = any
+  To extends string,
+  Before extends string = ''
+> = From extends ''
+  ? S
+  : S extends `${Before}${infer L}${From}${infer R}`
+  ? ReplaceAll<`${Before}${L}${To}${R}`, From, To, `${Before}${L}${To}`>
+  : S
