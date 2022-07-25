e1cf2c14fbe3ea92638a5c2374bbebd762702272
Kevin Ingles <kfabian@applaudostudios.dev>
2022-07-24 21:01:06 -0600

    ✨ Add trim challenge

--- questions/medium/108-medium-trim/template.ts
+++ questions/medium/108-medium-trim/template.ts
@@ -1,1 +1,6 @@
-export type Trim<S extends string> = any
+export type Whitespace = ' ' | '\n' | '\t'
+type Trim<S> = S extends `${Whitespace}${infer R}`
+  ? Trim<R>
+  : S extends `${infer L}${Whitespace}`
+  ? Trim<L>
+  : S
